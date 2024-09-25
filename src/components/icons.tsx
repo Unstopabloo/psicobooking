f3cb9b20016f3ab957b451fdeb22fd574ea3eeb1
Pablo Oyarce <pablooyarceramirez@gmail.com>
2024-09-24 23:52:45 -0300

    new landing

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -154,9 +154,10 @@ export const ChevronLeftIcon = (props: React.SVGProps<SVGSVGElement>) => (
 );
 
 export const MenuIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} className="stroke-[#2B303A] dark:stroke-white" fill={"none"} {...props}>
-    <path d="M4 8.5L20 8.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-    <path d="M4 15.5L20 15.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+    <path d="M10 5L20 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M4 12L20 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M4 19L14 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
   </svg>
 );
 
